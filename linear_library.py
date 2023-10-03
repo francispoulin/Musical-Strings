6e456d0321aa3f8a456f8a013000f5fdf22b05e9
sarah.toshiko <sarahtmoser@gmail.com>
2023-10-03 10:14:59 -0400

    add files for only linear model

---
+++ linear_library.py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python
+
+import glob
+from scipy import fftpack
+import numpy as np                     # Import Libraries
+import matplotlib.pyplot as plt
+import os                              # to delete the png files
+import subprocess                      # needed for movie
+import sys
+
+class parameters:
+    def __init__(self, N, L, dx, dt, tf, ts, m, Nt, npt, nsv, skip, c2_t, kt, method):
+        self.N  = N
+        self.L  = L
+        self.dx = dx
+        self.dt = dt
+        self.tf = tf
+        self.ts = ts
+        self.m = m
+        self.Nt = Nt
+        self.npt = npt
+        self.nsv = nsv
+        self.skip = skip
+        self.c2_t = c2_t
+        self.kt = kt
+        self.method = method
+
+def merge_to_mp4(frame_filenames, movie_name, fps=12):
+
+    f_log = open("output_files/ffmpeg.log", "w")
+    f_err = open("output_files/ffmpeg.err", "w")
+    cmd = ['ffmpeg', '-framerate', str(fps), '-i', frame_filenames, '-y', 
+            '-q', '1', '-threads', '0', '-pix_fmt', 'yuv420p', movie_name]
+    subprocess.call(cmd, stdout=f_log, stderr=f_err)
+    # remove the frame PNGs
+    for f in glob.glob("figures/frame_*.png"):
+        os.remove(f)
+    f_log.close()
+    f_err.close()
+
+def dfdx(f,dx):                         # A difference function (positive direction)
+    return (f[1:] - f[0:-1])/dx
+  
+def flux_wave_eqn(soln, parms):
+    dx = parms.dx           
+    c2_t = parms.c2_t
+    N = parms.N
+
+    dudx = dfdx(soln[0, :], dx)
+    
+    flux_v = np.zeros(N+1)
+
+    flux_v[1:-1] = c2_t * dfdx(dudx, dx) 
+
+
+    flux = np.vstack([soln[1, :], flux_v])
+              
+    return flux
+
+def plot_soln(x, xs, soln, spec, parms, fig, axs, movie, ii):  # TODO
+    L  = parms.L
+    kt = parms.kt
+
+    axs["TopLeft"].cla()
+    axs["BottomLeft"].cla()
+    axs["Right"].cla()
+
+    t = ii*parms.dt
+    fig.suptitle('Displacements in Elastic Rod at t = %7.7f' % t)
+
+    axs["TopLeft"].plot(x, soln[0, :], 'dodgerblue', linewidth=3)
+    axs["TopLeft"].set_xlim([0, L])
+    axs["TopLeft"].set_title("Displacement")
+    axs["TopLeft"].set_ylim([-0.0025, 0.0025])
+    axs["TopLeft"].grid(True)
+
+    axs["BottomLeft"].plot(x, soln[1, :], 'dodgerblue', linewidth=3)  # linear v
+    axs["BottomLeft"].set_title("Velocity")
+    axs["BottomLeft"].set_ylim([-16.0, 16.0])
+    axs["BottomLeft"].grid(True)
+
+    # power spectrum plots
+    axs["Right"].plot(kt, spec[0, :], color="deeppink", marker=".", linestyle="-")
+    axs["Right"].set_xlabel("frequency (Hz)")
+    axs["Right"].set_ylabel("f hat")
+    axs["Right"].set_title("Spectrum")
+    axs["Right"].grid(True)
+
+    plt.draw()
+    plt.pause(0.01)
+
+    if movie:
+        plt.savefig('figures/frame_{0:04d}.png'.format(int(ii/parms.m)), dpi=200)
+
+
+def plot_hovmoller(x, soln_save, parms):
+    nsave = round(parms.tf/parms.ts) + 1
+    fig, axs = plt.subplots(1, 2, sharey=True)     # Hovmoller plots of displacements
+    fig.suptitle("Hovmoller plots of wave eqns")
+    tts  = np.arange(nsave)*parms.ts
+
+    hplot1 = axs[0].pcolor(x, tts, np.transpose(soln_save[0,:,:]))
+    fig.colorbar(hplot1, ax=axs[0], extend='max')
+    axs[0].set_xlim([-parms.L/2, parms.L/2])
+    axs[0].set_ylim([tts[0], tts[-1]])
+    axs[0].set_title('displacement')
+
+    hplot2 = axs[1].pcolor(x, tts, np.transpose(soln_save[1,:,:]))
+    fig.colorbar(hplot2, ax=axs[1], extend='max')
+    axs[1].set_xlim([-parms.L/2, parms.L/2])
+    axs[1].set_ylim([tts[0], tts[-1]])
+    axs[1].set_title('velocity')
+
+    plt.savefig("figures/linear_hovmoller_plot_displacement.png")
+
+def output_info(parms):
+    cfl  = parms.c2_t*parms.dt/parms.dx
+
+    print("Solution to the one-dimensional wave equations")
+    print("==============================================")
+    print("Parameters (space):   L = ", parms.L, " N = ", parms.N, " dx = ", parms.dx)
+    print("Parameters (time):    tf = ", parms.tf, "  dt = ", parms.dt, " ts = ", parms.ts)
+    print(" ")
+    print("Parameters (speed):      c2_t = ", parms.c2_t)
+    print('Parameters (cfl):        cfl = ', cfl)
+    print(" ")
+    print("Plots will show ")
+    print("     1) displacements: uL, uN, wL, wN, pN")
+    print("     2) velocities:    vL, vN, sL, sN, vpN   ")
+
+    if cfl > 0.5:
+        print("The CFL paramter is greater than one.")
+        print("Please try again but reduce dt so that CFL < -0.5")
+        sys.exit('Stopping code!')
+
+def compute_k(N, dx, c):
+    kodd = fftpack.fftfreq(2*N, d=dx)
+    freq = kodd*c
+    freqh = freq[0:N]
+
+    return freqh
+
+def compute_fhat(soln, N):
+    fodd = np.hstack([soln, 0, -np.flipud(soln[1:])])
+    fodd_hat = fftpack.fft(fodd)
+    fhat = fodd_hat[0:N]
+
+    return fhat
+
+def fhat_all(soln_array, N):
+    """ i say all but really only the displacements """
+    fhat = np.zeros((1, N))
+
+    # transverse
+    fhat[0, :] = np.abs(compute_fhat(soln_array[0, :], N))
+
+    return fhat
+    
+def calculate_soln(x, xs, soln, soln_save, spec_save, parms, fig, axs, movie):
+
+    dt = parms.dt
+    method = parms.method
+
+    NLnm = method(soln, parms)           # Euler step
+    soln = soln + dt*NLnm;
+
+    NLn  = method(soln, parms)           # AdamsBashforth2 step
+    soln = soln + 0.5*dt*(3*NLn - NLnm)
+
+    count_save = 1
+    count_spec = 1
+    for ii in range(3,parms.Nt+3):                       # AdamsBashforth3 step
+        t    = ii*dt   
+        NL   = method(soln, parms);
+        soln = soln + dt/12*(23*NL - 16*NLn + 5*NLnm)
+
+        if ii%parms.npt==0:
+            # compute spectrum
+            spec = fhat_all(soln, parms.N)
+            spec_save[:, :, count_spec] = spec[:, :]
+            count_spec += 1
+
+            print('Plot at t = %6.7f' % t)
+            plot_soln(x, xs, soln, spec, parms, fig, axs, movie, ii)
+    
+        if ii%parms.nsv==0:
+            soln_save[:,:,count_save] = soln[:,:]
+            count_save += 1
+
+        NLnm, NLn = NLn, NL                        # Reset fluxes
+    return soln_save, spec_save
---
+++ linear_wave_eqn.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+#
+######################################################################
+#                                                                    #
+#  linear_wave_eqn.py                                                #
+#                                                                    #
+# Written Sep 29, 2023 by Francis J. Poulin and Sarah T. Moser       #
+#                                                                    # 
+######################################################################
+#
+# Methods to run this script:
+#  1) from the terminal using
+#     $ python3 linear_wave_eqn.py
+#
+#  2) after starting python3, use
+#  >>> import linear_wave_eqn
+
+# soln contains the solution in the following order: u, v
+# u = transverse displacement; v = transverse velocity
+
+# all the fields are defined on the cell edges x
+
+# Zero Dirichlet boundary conditions are imposed on all fields
+# u(0, t) = 0 = u(L, t)
+# v(0, t) = 0 = v(L, t)
+
+### import standard libraries
+import numpy as np                             # numerical library
+import matplotlib.pyplot as plt                # plotting library
+from pathlib import Path
+
+### import personal libraries
+import linear_library as lib
+
+# options to make the movie
+movie = True                                   # switch to make an animation
+movie_name = 'output_files/linear_wave_eqn_movie.mp4'
+
+# save data
+outfile = "output_files/linear_soln_data.npy"
+outfile_spec = "output_files/linear_spec_data.npy"
+
+### Input parameters
+L    = 0.961                                   # length of domain                
+N    = 25                                      # number of grid points
+dx   = L/N                                     # grid spacing
+c2_t = 1.13e5                                  # transverse wave speed (squared)
+
+t0, tf  = 0, 0.1                             # initial time, final time
+dt, ts  = 1e-10, 5e-6                          # time steps soln and output
+m       = 1e5                                  # multiplication factor for tp and movie
+tp      = dt*m                                 # time step for plotting
+
+### Compute Parameters
+Nt  = int(tf/dt)                               # mumber of time steps
+npt = int(tp/dt)                               # mumber of time steps to plot
+nsv = int(ts/dt)                               # mumber of time steps to save
+
+kt = lib.compute_k(N, dx, np.sqrt(c2_t))
+
+### Make output directories
+Path("figures/").mkdir(parents=True, exist_ok=True)
+Path("output_files/").mkdir(parents=True, exist_ok=True)
+
+### Store parameters in a class then output some info
+parms = lib.parameters(N = N, L = L, dx = dx, \
+                   dt = dt, tf = tf, ts = ts, m = m, Nt = Nt, npt = npt, nsv = nsv, skip = 5, \
+                   c2_t = c2_t, kt = kt, method = lib.flux_wave_eqn)
+lib.output_info(parms)
+
+### Initial Conditions with plot: uL, vL, wL, sL, uN, vN, wN, sN, uT, vT, wT, sT, pT, vpT
+x    = np.linspace(0, L, N+1)          # define grids (staggered grid)
+xs   = np.linspace(0 + dx/2, L - dx/2, N)     # staggered grid
+# ICs: initial velocity
+# 0.5 -> 0
+soln = np.vstack([0*x, 15.0*np.exp(-((x-L/2)**2)/(L/20)**2)])
+
+### Store data to plot later
+soln_save = np.zeros((2, N+1, round(tf/ts) + 1))
+soln_save[:,:,0] = soln
+
+spec_save = np.zeros((2, N, round((tf - t0) / tp) + 1))  # array to save spectrum values
+spec = lib.fhat_all(soln, N)
+spec_save[:, :, 0] = spec
+
+### Start plotting snapshots
+#fig, axs = plt.subplots(2, 2, sharex=False, figsize=(12, 5))      
+fig = plt.figure(constrained_layout=True)
+axs = fig.subplot_mosaic([["TopLeft", "Right"], ["BottomLeft", "Right"]])
+lib.plot_soln(x, xs, soln, spec, parms, fig, axs, movie, 0)
+
+### Calculate the solution
+soln_save, spec_save = lib.calculate_soln(x, xs, soln, soln_save, spec_save, parms, fig, axs, movie)
+plt.savefig("figures/linear_final_displacement.png")
+plt.show()
+
+### Save the data into a file
+with open(outfile, "wb") as f:
+    np.save(f, soln_save)
+
+with open(outfile_spec, "wb") as f:
+    np.save(f, spec_save)
+
+### Make animation
+if movie:
+    lib.merge_to_mp4('figures/frame_%04d.png', movie_name)
+
+## Hovmoller plots of the solution and save
+lib.plot_hovmoller(x, soln_save, parms)
